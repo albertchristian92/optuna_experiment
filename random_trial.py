9657c6d8e7076bc7991f4d105fa60a319236f28c
Albert Christian <albert.christian92@gmail.com>
2021-10-04 20:00:05 +0800

    Update random_trial.py

--- random_trial.py
+++ random_trial.py
@@ -1,12 +1,10 @@
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
-from sklearn.ensemble import RandomForestClassifier as cuRFC
+from cuml.ensemble import RandomForestClassifier as cuRFC
+from sklearn.ensemble import RandomForestClassifier as skruf
 from sklearn.metrics import accuracy_score, make_scorer
 from sklearn.model_selection import train_test_split
-
-import optuna
 import numpy as np
-# Load the data
 
 df = pd.read_csv('UCI_Credit_Card.csv')
 df = df.rename(columns={'default.payment.next.month': 'def_pay',
@@ -27,29 +25,11 @@ y = df['def_pay'].copy()
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
 
 
-# this means we will train on 80% of the data and test on the remaining 20%.
-
-# %%
-
-# forest_upsampled = cuRFC(bootstrap=True, class_weight=None, criterion='gini',
-#             max_depth=None, max_features='auto', max_leaf_nodes=None,
-#             min_impurity_decrease=0.0,
-#             min_samples_leaf=1, min_samples_split=2,
-#             min_weight_fraction_leaf=0.0, n_estimators=600, n_jobs=-1,
-#             oob_score=False, random_state=None, verbose=0,
-#             warm_start=False)
-
-forest_upsampled = cuRFC(bootstrap=True, class_weight=None, criterion='gini',
-            max_depth=None, max_features='auto', max_leaf_nodes=None,
-            min_impurity_decrease=0.0, min_impurity_split=None,
-            min_samples_leaf=1, min_samples_split=2,
-            min_weight_fraction_leaf=0.0, n_estimators=600, n_jobs=-1,
-            oob_score=False, random_state=None, verbose=0,
-            warm_start=False)
+forest_upsampled = skruf(bootstrap= True,max_depth= 2, max_features= 'auto', n_estimators= 452)
 
 
-gbm = forest_upsampled.fit(X_train, y_train)
-predictions = gbm.predict(X_test)
+gbm = forest_upsampled.fit(np.float32(X_train), np.float32(y_train))
+predictions = gbm.predict(np.float32(X_test))
 accuracy = accuracy_score(y_test, y_pred=predictions)
 
 print(accuracy)
